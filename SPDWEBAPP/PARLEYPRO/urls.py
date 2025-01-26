fabb1e151ab4d97ba0a6f512dbaffa66669e1c23
RJM <Rmorr2001@gmail.com>
2025-01-26 17:55:28 -0600

    parley pro is now ganster style

--- SPDWEBAPP/PARLEYPRO/urls.py
+++ SPDWEBAPP/PARLEYPRO/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.change_vote, name="poll"),
-
+    path('', views.poll_index, name="poll"),
+    path('start_vote/',views.start_vote, name="start_vote"),
+    path('past_polls/', views.past_polls, name='past_polls'),
     #path('logout/', views.logoutUser, name="logout"),
 ]
