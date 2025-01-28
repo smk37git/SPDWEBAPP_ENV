88cef51bfc63533d03790198655ba22e0f77460c
Arthur Schieszer <67496505+TheDeadCell@users.noreply.github.com>
2025-01-27 23:49:48 -0600

    DATABASE FILE IS NOW DETECTED FROM THE FOLDER ABOVE THE GIT REPO.

--- SPDWEBAPP/SPDWEBAPP_CORE/settings.py
+++ SPDWEBAPP/SPDWEBAPP_CORE/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'SPDWEBAPP_CORE.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': Path(BASE_DIR).parent.parent / 'db.sqlite3',
     }
 }
 
