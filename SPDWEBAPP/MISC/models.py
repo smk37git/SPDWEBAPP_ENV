c95e3df47c22a536ad430dc66873ecac9d834763
smk37git <sdmain3@gmail.com>
2025-01-28 13:35:26 -0600

    Added Day Light Savings Model to MISC/models.py

--- SPDWEBAPP/MISC/models.py
+++ SPDWEBAPP/MISC/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Day Light Savings Model
+class DayLightSavingsAlert(models.Model):
+    is_active = models.BooleanField(default=True)
+    custom_alert = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        name = "Day Light Savings Settings"
+        name_plural = "Day Light Savings Settings"
