6474865cfcfacf4353168e9ab61ecd9c683a5cd4
Barrett Keegan <142939405+barrettckeegan@users.noreply.github.com>
2025-02-01 03:28:10 +0000

    Profile fix

--- SPDWEBAPP/AUTHENTICATE/admin.py
+++ SPDWEBAPP/AUTHENTICATE/admin.py
@@ -1,6 +1,63 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from django import forms
+from .models import Brother_Profile, Role, Major
 
-from .models import *
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Profile', {'fields': ('brother_profile',)}),
+    )
+    readonly_fields = ('brother_profile',)
+    filter_horizontal = ()
 
-admin.site.register(Brother_Profile)
+    def brother_profile(self, obj):
+        return obj.brother_profile.profileImage
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.prefetch_related('brother_profile')
+        return qs
+
+    def brother_profile(self, obj):
+        return obj.brother_profile.profileImage
+
+    brother_profile.short_description = 'Profile Image'
+
+class Brother_ProfileForm(forms.ModelForm):
+    new_major = forms.CharField(required=False)
+
+    class Meta:
+        model = Brother_Profile
+        fields = ('majors',)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if self.cleaned_data['new_major']:
+            major, created = Major.objects.get_or_create(name=self.cleaned_data['new_major'])
+            instance.majors.add(major)
+        if commit:
+            instance.save()
+        return instance
+
+class Brother_ProfileAdmin(admin.ModelAdmin):
+    form = Brother_ProfileForm
+    list_display = ('user', 'profileImage', 'get_majors')
+
+    def get_majors(self, obj):
+        return ', '.join([major.name for major in obj.majors.all()])
+
+    get_majors.short_description = 'Majors'
+
+class MajorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+User  = get_user_model()
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Brother_Profile, Brother_ProfileAdmin)
 admin.site.register(Role)
+admin.site.register(Major, MajorAdmin)
--- SPDWEBAPP/AUTHENTICATE/models.py
+++ SPDWEBAPP/AUTHENTICATE/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Role(models.Model):
-    # Predefined role choices
     ROLE_CHOICES = [
         ('EXEC', 'Executive Board'),
         ('PLEDGE_BOARD', 'Pledge Board'),
@@ -13,32 +12,13 @@ class Role(models.Model):
         ('RUSH_CHAIR', 'Rush Chair'),
     ]
 
-    name = models.CharField(
-        max_length=50,
-        choices=ROLE_CHOICES,
-        unique=True
-    )
+    name = models.CharField(max_length=50, choices=ROLE_CHOICES, unique=True)
 
     def __str__(self):
         return self.get_name_display()
 
 class Major(models.Model):
-    MAJOR_CHOICES = [
-        ('Biological Engineering', 'Biological Engineering'),
-        ('Biomedical Engineering', 'Biomedical Engineering'),
-        ('Chemical Engineering', 'Chemical Engineering'),
-        ('Civil Engineering', 'Civil Engineering'),
-        ('Computer Engineering', 'Computer Engineering'),
-        ('Computer Science', 'Computer Science'),
-        ('Data Science', 'Data Science'),
-        ('Electrical Engineering', 'Electrical Engineering'),
-        ('Environmental Engineering', 'Environmental Engineering'),
-        ('Industrial Engineering', 'Industrial Engineering'),
-        ('Information Technology', 'Information Technology'),
-        ('Mechanical Engineering', 'Mechanical Engineering'),
-        ('Other', 'Other')
-    ]
-    name = models.CharField(max_length=100, choices=MAJOR_CHOICES, unique=True)
+    name = models.CharField(max_length=255, default='Unknown')
 
     def __str__(self):
         return self.name
@@ -49,7 +29,7 @@ class Brother_Profile(models.Model):
     lastName = models.CharField(max_length=200, null=True)
     dateCreated = models.DateTimeField(auto_now_add=True, null=True)
     roles = models.ManyToManyField(Role, blank=True)
-    profileImage = models.ImageField(upload_to='images/', null=True, blank=True)
+    profileImage = models.ImageField(upload_to='profile_images', blank=True, null=True)
     majors = models.ManyToManyField(Major, blank=True)
 
     def __str__(self):
