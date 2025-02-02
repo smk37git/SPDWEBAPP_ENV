e2e0a7e2651e6ecb35a6d95dba53568517a63848
Arthur Schieszer <67496505+TheDeadCell@users.noreply.github.com>
2025-02-02 00:35:00 -0600

    Parley Pro now Live-Updates every 5 seconds

--- SPDWEBAPP/PARLEYPRO/urls.py
+++ SPDWEBAPP/PARLEYPRO/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.poll_index, name="poll"),
     path('start_vote/',views.start_vote, name="start_vote"),
     path('past_polls/', views.past_polls, name='past_polls'),
+    path('poll-results/', views.poll_results, name='poll_results'), #Used for Live Update
+    #path('logout/', views.logoutUser, name="logout"),
 
     path('poll-results/', views.poll_results, name='poll_results'), #Used for Live Update
     #path('logout/', views.logoutUser, name="logout"),
