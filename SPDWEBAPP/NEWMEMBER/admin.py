c9e3855bf92e5dc499f9527e6500936b86ccbbfd
Arthur Schieszer <67496505+TheDeadCell@users.noreply.github.com>
2025-04-28 16:49:12 -0500

    Updated Adadmin panel to show marks properly

--- SPDWEBAPP/NEWMEMBER/admin.py
+++ SPDWEBAPP/NEWMEMBER/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import NewMember_Mark_Event_and_Request
 # Register your models here.
-admin.site.register(NewMember_Mark_Event_and_Request)
+#admin.site.register(NewMember_Mark_Event_and_Request)
+
+@admin.register(NewMember_Mark_Event_and_Request)
+class NewMemberMarkEventAndRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        'mark_event_title',
+        'target_user',
+        'requesting_user',
+        'mark_approval_status',
+        'mark_value',
+        'mark_event_date',
+    )
