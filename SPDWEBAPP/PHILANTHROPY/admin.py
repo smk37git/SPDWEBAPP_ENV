74af81c1c40b81c12b95f3f88607d8e79131d3db
Arthur Schieszer <67496505+TheDeadCell@users.noreply.github.com>
2025-04-28 16:53:47 -0500

    Updated Admin panel to show philanthropy properly

--- SPDWEBAPP/PHILANTHROPY/admin.py
+++ SPDWEBAPP/PHILANTHROPY/admin.py
@@ -3,4 +3,14 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Philanthropy_Hours_Event_and_Request)
+#admin.site.register(Philanthropy_Hours_Event_and_Request)
+
+@admin.register(Philanthropy_Hours_Event_and_Request)
+class PhilanthropyHoursEventAndRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        'philanthropy_event_title',
+        'user',
+        'philanthropy_approval_status',
+        'philanthropy_event_hours',
+        'philanthropy_event_date',
+    )
