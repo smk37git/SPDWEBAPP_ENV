69b18dd2275741c73ea62bd10c95bfa3e0f8aa8e
Arthur Schieszer <67496505+TheDeadCell@users.noreply.github.com>
2025-04-28 16:58:29 -0500

    Updated Admin panel to show polls properly

--- SPDWEBAPP/PARLEYPRO/admin.py
+++ SPDWEBAPP/PARLEYPRO/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Vote_Poll)
+#admin.site.register(Vote_Poll)
+
+@admin.register(Vote_Poll)
+class VotePollAdmin(admin.ModelAdmin):
+    list_display = (
+        'Vote_Poll_Name',
+        'created_by',
+        'Vote_Poll_Count_Yes',
+        'Vote_Poll_Count_No',
+        'Vote_Poll_Start',
+        'Vote_Poll_End',
+    )
