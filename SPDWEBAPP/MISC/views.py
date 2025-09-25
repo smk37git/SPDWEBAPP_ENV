c126ae6b523233e81264b16ab139400981f8f8ba
smk37git <sdmain3@gmail.com>
2025-09-25 16:17:18 -0500

    Test Commit post clone

--- SPDWEBAPP/MISC/views.py
+++ SPDWEBAPP/MISC/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 # Format the message so that there are linebreaks
-message = f"Daylight savings Time is on {next_change.strftime('%B %d, %Y')}. Clocks will move {direction} one hour.\n\nClock will move forward an hour."
+message = f"Daylight savings Time is on {next_change.strftime('%B %d, %Y')}. Clocks will move {direction} one hour\n\nClock will move forward an hour"
 
 # Replace newlines with <br> tags to 'copy' a linebreak
 message_with_linebreaks = message.replace('\n', '<br>')
